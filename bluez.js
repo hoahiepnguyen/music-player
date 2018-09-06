196f73e44f80d7c30f659e32e75be6d36e54a0e7
Hiep Nguyen <hiepnguyen@Hieps-mbp.local>
2018-09-06 23:24:24 +0700

    tested bluetooth

--- bluez.js
+++ bluez.js
@@ -1,13 +1,20 @@
 const Bluez = require('bluez')
 const bluetooth = new Bluez()
+const Agent = require('./Agent.js')
+const exec = require("child_process").exec;
 
 bluetooth.init().then(async() => {
+
     // listen on first bluetooth adapter
     const adapter = await bluetooth.getAdapter('hci0');
     await adapter.Powered('on');
     await adapter.Discoverable('on')
     console.log("Powered and Discoverable on");
 
-    await bluetooth.registerDefaultAgent()
+    // const obj = 
+    // const agent = new Agent('/test/agent', obj);
+    // await bluetooth.registerAgent(agent, "NoInputNoOutput")
+    // await bluetooth.registerDefaultAgent()
+    exec('python ./agent.py')
     console.log('Agent registered');
- })
+})
